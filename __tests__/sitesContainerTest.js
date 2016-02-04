13048484dc374f7709ac6d5c432644612ab5843c
Robin Drexler <drexler.robin@gmail.com>
2016-02-04 23:13:08 +0100

    Add test for filter

--- __tests__/sitesContainerTest.js
+++ __tests__/sitesContainerTest.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+function createSite(title, url) {
+  return {title, url};
+}
 describe('Sites Container', function() {
   jest.dontMock('../src/components/SitesContainer');
 
@@ -14,5 +17,33 @@ describe('Sites Container', function() {
     var sitesComponents = TestUtils.scryRenderedComponentsWithType(siteContainer, Site);
 
     expect(sitesComponents.length).toEqual(3);
+  });
+
+  it('filters sites by title', function() {
+    var sites = [
+      createSite('Camel', ''),
+      createSite('I do match', ''),
+      createSite('Cucumber', '')
+    ];
+
+    var filter = 'i do mat';
+    var siteContainer = TestUtils.renderIntoDocument(<SitesContainer filter={filter} sites={sites} site={Site} limit={9}/>);
+    var sitesComponents = TestUtils.scryRenderedComponentsWithType(siteContainer, Site);
+
+    expect(sitesComponents.length).toEqual(1);
+  });
+
+  it('filters sites by url', function() {
+    var sites = [
+      createSite('', 'http://google.com/1'),
+      createSite('', 'http://facebook.com/'),
+      createSite('', 'http://google.com/2')
+    ];
+
+    var filter = 'google';
+    var siteContainer = TestUtils.renderIntoDocument(<SitesContainer filter={filter} sites={sites} site={Site} limit={9}/>);
+    var sitesComponents = TestUtils.scryRenderedComponentsWithType(siteContainer, Site);
+
+    expect(sitesComponents.length).toEqual(2);
   });
 });
